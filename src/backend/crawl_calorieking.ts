66a87e589cdd6bb5ff37cb5b1f2d36160d71eb54
Gianluca Venturini <gianluca@stellarite.io>
2020-08-31 20:45:42 -0700

    Cached results

--- src/backend/crawl_calorieking.ts
+++ src/backend/crawl_calorieking.ts
@@ -1,7 +1,23 @@
 import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda";
 import * as puppeteer from 'puppeteer-core';
 import * as chromium from 'chrome-aws-lambda';
+import * as Lru from 'lru-cache';
 
+interface Stats {
+    fat: number;
+    carbs: number;
+    fiber: number;
+    protein: number;
+    satFat: number;
+    polyUnsatFat: number;
+    monoUnsatFat: number;
+    sugars: number;
+}
+
+const cache = new Lru<string, Stats>({
+    max: 10_000,
+    maxAge: 1000 * 60 * 60 * 24
+});
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
     console.log('event', event);
@@ -15,35 +31,36 @@ export const handler = async (event: APIGatewayProxyEvent, context: Context): Pr
     }
 
     const url = queries.url;
-    console.log('start', JSON.stringify(event.queryStringParameters));
+    let stats = cache.get(url);
 
-    const browser = await puppeteer.launch({
-        executablePath: await chromium.executablePath,
-        args: chromium.args,
-        defaultViewport: { width: 400, height: 3000 }, // necessary to include all the params in the visible part of the screen
-        headless: chromium.headless
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
+    if (!stats) {
+        console.log('start', JSON.stringify(event.queryStringParameters));
 
-    await mouseDownElement(page, '//div[./p[text() = "Serving"]]/div/div');
-    await clickElement(page, '//li[text()="g"]');
-    await inputElement(page, '//div[./p[text()="Quantity"]]//input', '100');
-    // await new Promise(resolve => setTimeout(() => resolve(), 10));
+        const browser = await puppeteer.launch({
+            executablePath: await chromium.executablePath,
+            args: chromium.args,
+            defaultViewport: { width: 400, height: 3000 }, // necessary to include all the params in the visible part of the screen
+            headless: chromium.headless
+        });
+        const page = await browser.newPage();
+        await page.goto(url);
 
-    await page.waitForXPath('//tr[./th/span[text()="Fat"]]/td/span');
-    const fat = await getTextElement(page, '//tr[./th/span[text()="Fat"]]/td/span');
-    const carbs = await getTextElement(page, '//tr[./th/span[text()="Carbs"]]/td/span');
-    const fiber = await getTextElement(page, '//tr[./th/span[text()="Fiber"]]/td/span');
-    const protein = await getTextElement(page, '//tr[./th/span[text()="Protein"]]/td/span');
-    const satFat = await getTextElement(page, '//tr[./th/a[text()="Saturated Fat"]]/td');
-    const polyUnsatFat = await getTextElement(page, '//tr[./th/a[text()="Polyunsaturated Fat"]]/td');
-    const monoUnsatFat = await getTextElement(page, '//tr[./th/a[text()="Monounsaturated Fat"]]/td');
-    const sugars = await getTextElement(page, '//tr[./th/a[text()="Sugars"]]/td');
+        await mouseDownElement(page, '//div[./p[text() = "Serving"]]/div/div');
+        await clickElement(page, '//li[text()="g"]');
+        await inputElement(page, '//div[./p[text()="Quantity"]]//input', '100');
+        // await new Promise(resolve => setTimeout(() => resolve(), 10));
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
+        await page.waitForXPath('//tr[./th/span[text()="Fat"]]/td/span');
+        const fat = await getTextElement(page, '//tr[./th/span[text()="Fat"]]/td/span');
+        const carbs = await getTextElement(page, '//tr[./th/span[text()="Carbs"]]/td/span');
+        const fiber = await getTextElement(page, '//tr[./th/span[text()="Fiber"]]/td/span');
+        const protein = await getTextElement(page, '//tr[./th/span[text()="Protein"]]/td/span');
+        const satFat = await getTextElement(page, '//tr[./th/a[text()="Saturated Fat"]]/td');
+        const polyUnsatFat = await getTextElement(page, '//tr[./th/a[text()="Polyunsaturated Fat"]]/td');
+        const monoUnsatFat = await getTextElement(page, '//tr[./th/a[text()="Monounsaturated Fat"]]/td');
+        const sugars = await getTextElement(page, '//tr[./th/a[text()="Sugars"]]/td');
+
+        stats = {
             fat,
             carbs,
             fiber,
@@ -52,7 +69,15 @@ export const handler = async (event: APIGatewayProxyEvent, context: Context): Pr
             polyUnsatFat,
             monoUnsatFat,
             sugars
-        })
+        }
+        cache.set(url, stats);
+    } else {
+        console.log('Stats retrieved from cache');
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(stats)
     }
 }
 
